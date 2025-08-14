d43b5606caa2c303cd7f519bcf6996d22af012e3
Jaogui9 <jaoguistenico05@gmail.com>
2025-08-14 13:46:54 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'bora-app-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'logo_bora_la_pwa_192.png',
+  'logo_bora_la_pwa_512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
